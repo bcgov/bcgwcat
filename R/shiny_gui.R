4619d6e33223d17f9e49b7b9e00d0491ce68f8cc
Steffi LaZerte <steffi@steffi.ca>
2019-06-03 17:19:35 -0500

    Export gui

--- R/shiny_gui.R
+++ R/shiny_gui.R
@@ -1,3 +1,10 @@
+#' REMS to Aquachem GUI
+#' Shiny app to launch an interactive interface for getting and formating data.
+#'
+#' @examples
+#' \dontrun{ac_gui()}
+#'
+#' @export
 ac_gui <- function() {
   appDir <- system.file("shiny", package = "rems2aquachem")
   if (appDir == "") {
