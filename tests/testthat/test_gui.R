2ca3a42db4ab32e3af4bbc3b5b7bec59b48dda3c
Steffi LaZerte <steffi@steffi.ca>
2019-06-12 16:10:43 -0500

    Update tests

--- tests/testthat/test_gui.R
+++ tests/testthat/test_gui.R
@@ -5,6 +5,6 @@ library(shinytest)
 test_that("ac_gui() works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
-
-  expect_pass(testApp("apps/ac_gui", compareImages = FALSE))
+  appdir <- system.file(package = "rems2aquachem", "shiny")
+  expect_pass(testApp(appdir, compareImages = FALSE))
 })
