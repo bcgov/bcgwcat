1ae725a6ef85daca9b65710515e92bf7d70d8f24
steffilazerte <steffi@steffi.ca>
2020-11-26 14:34:08 -0600

    Update data source and params list

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,13 +1,15 @@
 params <- readr::read_csv("data-raw/params_list.csv")
 
-library(bcdata)
+httr::GET("https://s3.ca-central-1.amazonaws.com/gwells-export/export/gwells.zip",
+          httr::write_disk("gwells.zip", overwrite = TRUE), httr::progress())
+unzip("gwells.zip", files = "well.csv", overwrite = TRUE)
 
-#bcdc_search("WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW")
-
-ow <- bcdc_get_data("e4731a85-ffca-4112-8caf-cb0a96905778") %>%
-  as.data.frame() %>%
-  dplyr::select(ow = OBSERVATION_WELL_NUMBER, ems_id = CHEMISTRY_SITE_ID) %>%
+ow <- readr::read_csv("well.csv", guess_max = 200000) %>%
+  dplyr::select(ow = observation_well_number, ems_id = ems) %>%
   dplyr::filter(!is.na(ow), !is.na(ems_id))
 
 
 usethis::use_data(params, ow, internal = TRUE, overwrite = TRUE)
+
+unlink("gwells.zip")
+unlink("well.csv")
